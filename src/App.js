5501eb98c3a44f62f9c1b2cbd9d3d525bfadf7ca
tplai <tplai@umich.edu>
2020-07-30 03:19:56 -0400

    Removed starter text

--- src/App.js
+++ src/App.js
@@ -9,10 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <KeyGenerator />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        </header>
+      </header>
     </div>
   );
 }
