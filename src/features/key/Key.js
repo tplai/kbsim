46e8c7f3e4eaaab43e3979db50923c41fe0b4048
tplai <tplai@umich.edu>
2020-07-30 03:20:51 -0400

    Finished basic Key class and styling

--- src/features/key/Key.js
+++ src/features/key/Key.js
@@ -5,7 +5,7 @@ import styles from './Key.module.css';
 // keysize must be divisble by 9
 let keysize = 54;
 
-const Key = ({legend, sublegend, width, height, x, y}) => {
+const Key = ({legend, sublegend, width, height, x, y}) => (
   <div className={styles.keycap}>
     <div
       className={styles.keyborder}
@@ -19,52 +19,64 @@ const Key = ({legend, sublegend, width, height, x, y}) => {
     <div
       className={styles.keytop}
       style={{
-        left: x * keysize * width + keysize / 9,
-        top: y * keysize * height + keysize / 18,
+        left: x * keysize + keysize / 9,
+        top: y * keysize + keysize / 18,
         width: keysize * width - keysize * 2 / 9,
         height: keysize * height - keysize * 2 / 9,
       }}
     />
     <div
-      className={styles.keylabels}
+      className={styles.keylegends}
       style={{
-        left: x * keysize * width + keysize / 9,
-        top: y * keysize * height + keysize / 18,
+        left: x * keysize + keysize / 9,
+        top: y * keysize + keysize / 18,
         width: keysize * width - keysize * 2 / 9,
         height: keysize * height - keysize * 2 / 9,
       }}
     >
       <div
-        className={styles.keylabel}
+        className={styles.keylegend}
         style={{
           width: keysize * width - keysize * 3 / 9,
           maxWidth: keysize * width - keysize * 3 / 9,
-          height: keysize * height - keysize * 3/ 9,
+          height: keysize * height - keysize * 3 / 9,
         }}
       >
-        {legend}
+        <div
+          style={{
+            width: keysize * width - keysize * 3 / 9,
+            height: keysize * height - keysize * 3 / 9,
+          }}>
+          {legend}
+        </div>
       </div>
       <div
-        className={styles.keylabel}
+        className={styles.keysublegend}
         style={{
           width: keysize * width - keysize * 3 / 9,
-          maxWidth: keysize * width - keysize * 3 / 9,
-          height: keysize * height - keysize * 3/ 9,
+          height: keysize * height - keysize * 3 / 9,
         }}
       >
-        {sublegend}
+        <div
+          style={{
+            width: keysize * width - keysize * 3 / 9,
+            maxWidth: keysize * width - keysize * 3 / 9,
+            height: keysize * height - keysize * 3 / 9,
+          }}>
+          {sublegend}
+        </div>
       </div>
     </div>
   </div>
-}
+)
 
-Todo.propTypes = {
-  legend: PropTypes.func.isRequired,
-  sublegend: PropTypes.func.isRequired,
-  x: PropTypes.func.isRequired,
-  y: PropTypes.func.isRequired,
-  width: PropTypes.func.isRequired,
-  height: PropTypes.func.isRequired,
+Key.propTypes = {
+  legend: PropTypes.string.isRequired,
+  sublegend: PropTypes.string.isRequired,
+  x: PropTypes.number.isRequired,
+  y: PropTypes.number.isRequired,
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
 }
 
 export default Key
