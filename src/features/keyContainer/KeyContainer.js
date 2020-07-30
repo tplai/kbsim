4052cc78c419a7b5a6427bbc87ad0ffcaaae6079
tplai <thomas.lai920@gmail.com>
2020-07-30 12:15:26 -0400

    Added keyup support

--- src/features/keyContainer/KeyContainer.js
+++ src/features/keyContainer/KeyContainer.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import styles from './KeyContainer.module.css';
 
-
-
 const KeyContainer = ({children}) => {
-  const handleKeyDown = e => {
-    console.log(" XD");
+  const handleKeyDown = (e) => {
+    console.log(e.keyCode);
+  }
+  const handleKeyUp = (e) => {
+    console.log(e.keyCode);
   }
   return (
-    <div tabindex="0"
+    <div
       className={styles.keycontainer}
-      onKeyDown={handleKeyDown}
+      onKeyDown={e => handleKeyDown(e)}
+      onKeyUp={e => handleKeyUp(e)}
+      tabindex="0"
     >
       {children}
     </div>
