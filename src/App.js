fe663faa6f0f0846e6e339277bbd5dc9157554a0
tplai <tplai@umich.edu>
2020-07-30 08:15:31 -0400

    Removed default image

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import { KeyGenerator } from './features/keyGenerator/KeyGenerator';
 import './App.css';
+// <img src={logo} className="App-logo" alt="logo" />
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <KeyGenerator />
       </header>
     </div>
