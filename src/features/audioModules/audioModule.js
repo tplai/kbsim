845c32e59d266e07c1d24a27571b0c4e435e543d
tplai <thomas.lai920@gmail.com>
2020-08-04 05:23:37 -0400

    Added MX black support

--- src/features/audioModules/audioModule.js
+++ src/features/audioModules/audioModule.js
@@ -1,6 +1,7 @@
 import { holypanda } from './holypanda.js';
 import { blackink } from './blackink.js';
 import { redink } from './redink.js';
+import { mxblack } from './mxblack'
 import { cream } from './cream.js';
 import { boxnavy } from './boxnavy.js';
 
@@ -9,7 +10,7 @@ export const keySounds = {
   blackink: blackink,
   redink: redink,
   cream: cream,
-  // mxblack: mxblack,
+  mxblack: mxblack,
   boxnavy: boxnavy,
   // topre: topre,
 }
