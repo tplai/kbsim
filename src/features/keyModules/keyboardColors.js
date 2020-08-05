7d63d38986eebb8bcd566ee07a9cc06fa90799d1
tplai <thomas.lai920@gmail.com>
2020-08-05 17:13:25 -0400

    Added silver keyboard color

--- src/features/keyModules/keyboardColors.js
+++ src/features/keyModules/keyboardColors.js
@@ -1,13 +1,18 @@
 export const keyboardColors = [
   {
-    color: "gray",
-    caption: "Gray",
-    background: "linear-gradient(360deg, rgba(130,130,130,1) 0%, rgba(166,167,168,1) 75%)",
+    color: "silver",
+    caption: "Silver",
+    background: "linear-gradient(360deg, rgba(227,236,242,1) 0%, rgba(211,216,219,1) 75%)",
   },
   {
     color: "black",
     caption: "Black",
     background: "linear-gradient(360deg, rgba(65,65,65,1) 0%, rgba(42,42,42,1) 75%)",
+  },
+  {
+    color: "gray",
+    caption: "Gray",
+    background: "linear-gradient(360deg, rgba(130,130,130,1) 0%, rgba(166,167,168,1) 75%)",
   },
   {
     color: "white",
