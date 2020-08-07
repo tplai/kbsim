513ca62a30591821d12803b06504f6855a42ca90
tplai <thomas.lai920@gmail.com>
2020-08-06 21:04:51 -0400

    Added get random elements from array function

---
+++ src/features/typingModules/typingModules.js
@@ -0,0 +1,14 @@
+// get n random elements from an array
+export function getRandom(arr, n) {
+  let result = new Array(n),
+    len = arr.length,
+    taken = new Array(len);
+  if (n > len)
+    throw new RangeError("getRandom: more elements taken than available");
+  while (n--) {
+    let x = Math.floor(Math.random() * len);
+    result[n] = arr[x in taken ? taken[x] : x];
+    taken[x] = --len in taken ? taken[len] : len;
+  }
+  return result;
+}
