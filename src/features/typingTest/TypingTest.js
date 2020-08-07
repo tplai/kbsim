d63fd34a5eb4c85482b40c617dba848a4bb82091
tplai <thomas.lai920@gmail.com>
2020-08-06 21:06:10 -0400

    Added dynamic word component creation

--- src/features/typingTest/TypingTest.js
+++ src/features/typingTest/TypingTest.js
@@ -1,11 +1,29 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { wordList, shuffle } from './wordlist.js';
+import {
+  keyDown,
+  tick,
+  redo,
+  selectTime,
+  selectWords,
+} from './typingTestSlice.js';
+import Word from './../word/Word.js';
 import styles from './TypingTest.module.css';
 
 export function TypingTest() {
+  const words = useSelector(selectWords);
+  const timeLeft = useSelector(selectTime);
+
+  const dispatch = useDispatch();
 
   const [inputVal, setInput] = useState("");
+  const [startRace, setStartRace] = useState(false);
+
+  let wordObject = words.map((word, index) => {
+    return(
+      <Word key={index} focused={false} status={"default"} text={word}/>
+    )
+  });
 
   const handleKeyDown = (e) => {
     // console.log(e.target.value);
@@ -16,36 +34,32 @@ export function TypingTest() {
 
   }
 
+  // int
+  const parseSecond = (time) => {
+    let seconds = time % 60;
+    if (seconds >= 10) {
+      return toString(seconds);
+    }
+    else {
+      // if (toString(seconds).length == 1) {
+        // console.log(`0${seconds}`);
+        return `0${seconds}`;
+      // }
+    }
+  }
+
+  // <Word focused={true} status={"correct"} text={"lorem"}/>
+  // <Word focused={true} status={"incorrect"} text={"lorem"}/>
+  // <Word focused={false} status={"correct"} text={"lorem"}/>
+  // <Word focused={false} status={"blank"} text={"lorem"}/>
+  // <Word focused={false} status={"blank"} text={"lorem"}/>
+
   return (
     <div className={styles.typingcontainer}>
       <div className={styles.wordcontainer}>
         <div className={styles.wordarea}>
           <div className={styles.words}>
-            <span className={`${styles.word} ${styles.active}`}>lorum</span>
-            <span className={styles.word}>ipsum</span>
-            <span className={styles.word}>lorum</span>
-            <span className={styles.word}>ipsum</span>
-            <span className={styles.word}>lorum</span>
-            <span className={styles.word}>ipsum</span>
-            <span className={styles.word}>lorum</span>
-            <span className={styles.word}>ipsum</span>
-            <span className={styles.word}>lorum</span>
-            <span className={styles.word}>ipsum</span>
-            <span className={styles.word}>lorum</span>
-            <span className={styles.word}>ipsum</span>
-            <span className={styles.word}>lorum</span>
-            <span className={styles.word}>ipsum</span>
-            <span className={styles.word}>lorum</span>
-            <span className={styles.word}>ipsum</span>
-            <span className={styles.word}>lorum</span>
-            <span className={styles.word}>ipsum</span>
-            <span className={styles.word}>lorum</span>
-            <span className={styles.word}>ipsum</span>
-            <span className={styles.word}>lorum</span>
-            <span className={styles.word}>ipsum</span>
-            <span className={styles.word}>lorum</span>
-            <span className={styles.word}>ipsum</span>
-            <span className={styles.word}>ipsum</span>
+              {wordObject}
           </div>
         </div>
       </div>
@@ -53,13 +67,13 @@ export function TypingTest() {
         <input
           className={styles.typinginput}
           onChange={handleKeyDown}
-          autoFocus="true"
+          autoFocus={true}
           autoCorrect="off"
           autoCapitalize="off"
           spellCheck="false"
         />
         <span className={styles.toolbar}>
-          <span className={styles.time}>1:00</span>
+          <span className={styles.time}>{timeLeft ? timeLeft / 60 : "x"}:{timeLeft ? parseSecond(timeLeft) : "xx"}</span>
           <button disabled="">Redo</button>
         </span>
       </div>
