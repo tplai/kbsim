939b625b5f491d3ebefc1a350c7023531182c4b4
tplai <thomas.lai920@gmail.com>
2020-08-08 22:12:35 -0400

    Rotated out some layouts and set the default layout to 9009

--- src/features/keyModules/keyboardColors.js
+++ src/features/keyModules/keyboardColors.js
@@ -1,13 +1,13 @@
 export const keyboardColors = [
   {
+    color: "gray",
+    caption: "Gray",
+    background: "linear-gradient(137deg, rgba(182,186,185,1) 0%, rgba(178,178,178,1) 75%)",
+  },
+  {
     color: "black",
     caption: "Black",
     background: "linear-gradient(360deg, rgba(65,65,65,1) 0%, rgba(42,42,42,1) 75%)",
-  },
-  {
-    color: "gray",
-    caption: "Gray",
-    background: "linear-gradient(360deg, rgba(150,150,150,1) 0%, rgba(130,130,130,1) 75%)",
   },
   {
     color: "silver",
