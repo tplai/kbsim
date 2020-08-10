5b19a71b9c4786999cea0705e54eb4efb69314bd
tplai <thomas.lai920@gmail.com>
2020-08-09 22:32:21 -0400

    Removed alpaca and gateron ink red switches temporarily

--- src/features/audioModules/audioModule.js
+++ src/features/audioModules/audioModule.js
@@ -1,11 +1,11 @@
-import { alpaca } from './alpaca';
+// import { alpaca } from './alpaca';
 import { alpsblue } from './alpsblue';
 import { boxnavy } from './boxnavy';
 import { buckling } from './buckling';
 import { cream } from './cream';
 import { holypanda } from './holypanda';
 import { inkblack } from './inkblack';
-import { inkred } from './inkred';
+// import { inkred } from './inkred';
 import { mxblack } from './mxblack';
 import { mxblue } from './mxblue';
 import { mxbrown } from './mxbrown';
@@ -14,11 +14,11 @@ import { turquoise } from './turquoise';
 
 export const keySounds = [
   cream,
-  alpaca,
+  holypanda,
+  // alpaca,
   turquoise,
   inkblack,
-  inkred,
-  holypanda,
+  // inkred,
   mxblack,
   mxbrown,
   mxblue,
@@ -27,6 +27,27 @@ export const keySounds = [
   alpsblue,
   topre,
 ]
+
+// import { cream } from './cream';
+//
+// import('./alpaca').then(alpaca => {keySounds.push(alpaca)});
+// import('./alpsblue').then(alpsblue => {keySounds.push(alpsblue)});
+// import('./boxnavy').then(boxnavy => {keySounds.push(boxnavy)});
+// import('./buckling').then(buckling => {keySounds.push(buckling)});
+// // import('./cream').then(cream => {keySounds.push(cream)});
+// import('./holypanda').then(holypanda => {keySounds.push(holypanda)});
+// import('./inkblack').then(inkblack => {keySounds.push(inkblack)});
+// import('./inkred').then(inkred => {keySounds.push(inkred)});
+// import('./mxblack').then(mxblack => {keySounds.push(mxblack)});
+// import('./mxblue').then(mxblue => {keySounds.push(mxblue)});
+// import('./mxbrown').then(mxbrown => {keySounds.push(mxbrown)});
+// import('./topre').then(topre => {keySounds.push(topre)});
+// import('./turquoise').then(turquoise => {keySounds.push(turquoise)});
+//
+// export const keySounds = [
+//   cream
+// ]
+
 
 // sadly you can't import in the middle of a file
 // let audiofolder = "./../../assets/audiofolder";
