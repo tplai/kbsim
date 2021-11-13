4e9be212704d4f8294b854b7a6f7e20c7d76dba8
Thomas Lai <thomas.lai920@gmail.com>
2021-11-13 02:22:01 -0500

    Added empty themeProvider to project

---
+++ src/features/themeProvider/ThemeProvider.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { connect, useSelector, useDispatch } from 'react-redux';
+import {
+    toggleTheme,
+    selectTheme
+} from './themeProviderSlice.js';
+import store from '../store/store';
+
+store.dispatch(toggleTheme);
+
+function ThemeProvider({ children }) {
+    const theme = useSelector(selectTheme);
+    // console.log(theme);
+
+    return (
+        <div>
+            {children}
+        </div>
+    );
+}
+
+// function mapStateToProps(state) {
+    // return { theme }
+//   }
+  
+// export default connect(mapStateToProps)(ThemeProvider);
+
+export default ThemeProvider;
---
+++ src/features/themeProvider/themeProviderSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const themeProviderSlice = createSlice({
+    name: 'themeProvider',
+    initialState: {
+        theme: "dark",
+    },
+    reducers: {
+        toggleTheme: (state) => {
+            // state.theme = (state.theme == "light") ? "dark" : "light"
+            state.theme = "light";
+        },
+    },
+});
+
+export const { toggleTheme } = themeProviderSlice.actions;
+
+// state exports
+export const selectTheme = state => state.themeProvider.theme;
+
+export default themeProviderSlice.reducer;
+
