f6a1e6ab147acfd6e521717cad46ee08152fdb8d
Thomas Lai <thomas.lai920@gmail.com>
2021-11-13 02:21:44 -0500

    Added ThemeProviderSlice to store

--- src/features/store/store.js
+++ src/features/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import themeProviderReducer from '../themeProvider/themeProviderSlice';
 import keySimulatorReducer from '../keySimulator/keySimulatorSlice';
 import typingTestReducer from '../typingTest/typingTestSlice';
 
 export default configureStore({
   reducer: {
+    themeProvider: themeProviderReducer,
     keySimulator: keySimulatorReducer,
     typingTest: typingTestReducer,
   },
