4c149fc959ac1960f4b771d87b15e2d941203f4a
Thomas Lai <thomas.lai920@gmail.com>
2021-11-16 03:50:24 -0500

    Added dark theme toggle to header

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,13 +1,35 @@
 import React from 'react'
 import logosvg from './../../assets/images/logo.svg'
+import { BsMoonStarsFill as Moon, BsFillBrightnessAltHighFill as Sun } from "react-icons/bs"
+import { toggleTheme } from '../themeProvider/themeProviderSlice';
+import { connect } from 'react-redux';
 import styles from './Header.module.css';
 
-const Header = () => (
-  <div className={styles.headerContainer}>
-    <div className={styles.logo}>
-      <img src={logosvg} className={styles.logosvg} height="32" width="32"/> kbsim
+function Header({ theme, toggle }) {  
+  return (
+    <div className={styles.header}>
+      <div className={styles.headerContainer}>
+        <div className={styles.logo}>
+          <img src={logosvg} className={styles.logosvg} height="32" width="32"/> kbsim
+        </div>
+        <div className={styles.subnav} onClick={toggle}>
+        {theme === "light" ? <Moon className={styles.lightswitch}/> : <Sun className={styles.lightswitch}/>}
+        </div>
+      </div>
     </div>
-  </div>
-)
+  );
+}
 
-export default Header
+const mapStateToProps = (state) => {
+  return {
+      theme: state.themeProvider.theme
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    toggle: () => dispatch(toggleTheme())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
