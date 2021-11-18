1906904a326ef02fc9dae61f2ccb5a9c4814d7dd
Thomas Lai <thomas.lai920@gmail.com>
2021-11-18 01:53:53 -0500

    Added themeprovider styling

--- src/features/themeProvider/ThemeProvider.js
+++ src/features/themeProvider/ThemeProvider.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { palette } from '../theme/theme';
 import store from '../store/store';
+import styles from './ThemeProvider.module.css';
 
 function ThemeProvider({ children }) {
     document.body.style = `background: ${[palette[store.getState().themeProvider.current].background]};`;
     return (
-        <div>
+        <div class={styles.themeProvider}>
             {children}
         </div>
     );
