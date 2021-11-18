54eb008fa0b71e11ca6cd4ab650caa539ab84c2f
Thomas Lai <thomas.lai920@gmail.com>
2021-11-18 02:26:31 -0500

    Connected themeprovider to dark theme to fix flashing on refresh

--- src/features/themeProvider/ThemeProvider.js
+++ src/features/themeProvider/ThemeProvider.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { palette } from '../theme/theme';
 import store from '../store/store';
 import styles from './ThemeProvider.module.css';
 
-function ThemeProvider({ children }) {
+function ThemeProvider({ children, theme }) {
     document.body.style = `background: ${[palette[store.getState().themeProvider.current].background]};`;
     return (
-        <div className={styles.themeProvider}>
+        <div 
+          className={styles.themeProvider}
+          style={{
+              backgroundColor: theme.background
+          }}
+        >
             {children}
         </div>
     );
 }
 
-export default ThemeProvider;
+const mapStateToProps = (state) => {
+    return {
+        theme: state.themeProvider.theme
+    }
+  }
+
+export default connect(mapStateToProps)(ThemeProvider);
